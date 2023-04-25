446d240662a22f125dc0a695ac2f6f0a05c1c4b7
Krzysztof Farys <krzysztoffarys00@gmail.com>
2023-04-25 18:17:47 +0200

    added logs

--- skrypt.sh
+++ skrypt.sh
@@ -2,7 +2,13 @@
 
 if [[ "$1" == "--date" ]]; then
     echo "Dzisiaj jest $(date +%Y-%m-%d)"
+elif [[ "$1" == "--logs" ]]; then
+    for i in {1..100}; do
+        filename="log${i}.txt"
+        echo "Nazwa pliku: $filename" >> "$filename"
+        echo "Nazwa skryptu: $0" >> "$filename"
+        echo "Data utworzenia: $(date)" >> "$filename"
+    done
 else
     echo "Nieprawidłowe argumenty wywołania."
-    echo "Użycie: skrypt.sh --date"
 fi
