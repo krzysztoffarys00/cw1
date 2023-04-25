1809421fa4f2417c8ee5e2581788b135f384c2c7
Krzysztof Farys <krzysztoffarys00@gmail.com>
2023-04-25 18:22:37 +0200

    logs extended

--- skrypt.sh
+++ skrypt.sh
@@ -3,12 +3,16 @@
 if [[ "$1" == "--date" ]]; then
     echo "Dzisiaj jest $(date +%Y-%m-%d)"
 elif [[ "$1" == "--logs" ]]; then
-    for i in {1..100}; do
-        filename="log${i}.txt"
-        echo "Nazwa pliku: $filename" >> "$filename"
-        echo "Nazwa skryptu: $0" >> "$filename"
-        echo "Data utworzenia: $(date)" >> "$filename"
-    done
+    if [[ "$2" =~ ^[0-9]+$ ]]; then
+        for i in $(seq 1 "$2"); do
+            filename="log${i}.txt"
+            echo "Nazwa pliku: $filename" >> "$filename"
+            echo "Nazwa skryptu: $0" >> "$filename"
+            echo "Data utworzenia: $(date)" >> "$filename"
+        done
+    else
+        echo "Nieprawidłowy argument wywołania"
+    fi
 else
     echo "Nieprawidłowe argumenty wywołania."
 fi
