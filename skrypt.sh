d6afd8d03412f047609a9ce46d3454390ff07997
Krzysztof Farys <krzysztoffarys00@gmail.com>
2023-04-25 18:52:33 +0200

    added error functionality

--- skrypt.sh
+++ skrypt.sh
@@ -17,10 +17,19 @@ elif [[ "$1" == "--help" || "$1" == "-h" ]]; then
     echo "Użycie: skrypt.sh --date|--logs [N]"
     echo "--date: Wyświetla dzisiejszą datę w formacie YYYY-MM-DD."
     echo "--logs [N]: Tworzy N plików logu o nazwie logx.txt (dla x od 1 do N) z wpisem o nazwie, nazwie skryptu i dacie utworzenia."
-elif [[ "$1" == "--init"]]; then
+elif [[ "$1" == "--init" ]]; then
     url="https://github.com/krzysztoffarys00/cw1"	
     git clone $url
     export PATH="$PATH:$(pwd)"
+elif [[ "$1" == "--error" || "$1" == "-e" ]]; then
+    if [[ "$2" =~ ^[0-9]+$ ]]; then
+        for i in $(seq 1 "$2"); do
+            filename="errorx${i}.txt"
+            echo "Nazwa pliku: $filename" >> "$filename"
+        done
+    else
+        echo "Nieprawidłowy argument wywołania"
+    fi
 else
     echo "Nieprawidłowe argumenty wywołania."
 fi
